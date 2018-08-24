57273b702f3426af8bc698815996a6a443f11964
GUOAB <AbbyGuo@oocl.com>
2018-08-24 14:29:26 +0800

    [abby]add test for call statement given one rental which movie is regular,dayRented is 3

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -10,7 +10,7 @@ public class CustomerTest {
     private Customer customer = new Customer("abby");
 
     @Test
-    public void should_return_correst_statement_given_customer_has_no_rental() {
+    public void should_return_correct_statement_given_customer_has_no_rental() {
         String result = customer.statement();
         assertThat(result,is("Rental Record for abby\n" +
                 "Amount owed is0.0\n" +
@@ -18,7 +18,7 @@ public class CustomerTest {
     }
 
     @Test
-    public void should_return_correst_statement_given_customer_has_rent_one_rental() {
+    public void should_return_correct_statement_given_customer_regular_movie_has_rent_one_rental_and_day_rented_1() {
         Movie movie =new Movie("movie1Title",Movie.REGULAR);
         Rental rental = new Rental(movie,1);
         customer.addRental(rental);
@@ -28,5 +28,18 @@ public class CustomerTest {
                 "Amount owed is2.0\n" +
                 "You earned1 frequent renter points"));
     }
+
+    @Test
+    public void should_return_correct_statement_given_customer_regular_movie_has_rent_one_rental_and_day_rented_3() {
+        Movie movie =new Movie("movie1Title",Movie.REGULAR);
+        Rental rental = new Rental(movie,3);
+        customer.addRental(rental);
+        String result = customer.statement();
+        assertThat(result,is("Rental Record for abby\n" +
+                "\tmovie1Title\t3.5\n" +
+                "Amount owed is3.5\n" +
+                "You earned1 frequent renter points"));
+    }
+
 
 }
